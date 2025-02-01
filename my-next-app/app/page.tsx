98b99501cfe7c117d5c527ec3f997ac1066385bb
Anoop Mathew <anoop@nenosys.se>
2025-02-01 18:33:07 +0100

    inital commig

---
+++ my-next-app/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Next.js up and running!</h1>
+    </div>
+  );
+}
